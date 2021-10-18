0ee0e732e2aa5220bb1364ff8858fe64f2470373
Sophie <81225116+SophieKoenig@users.noreply.github.com>
2021-10-18 14:54:30 +0200

    add Berlin Time

--- javascript/script.js
+++ javascript/script.js
@@ -6,8 +6,17 @@ const headerEp = document.querySelector(".header");
 const btnGameEp = document.querySelector(".btn-game");
 
 btnNavEp.addEventListener("click", function () {
-	headerEp.classList.toggle("nav-open");
+  headerEp.classList.toggle("nav-open");
 });
 btnGameEp.addEventListener("click", function () {
-	headerEp.classList.toggle("nav-game-open");
+  headerEp.classList.toggle("nav-game-open");
 });
+
+function timeBerlin() {
+  var timeLA = new Date().toLocaleString("en-US", {
+    timeZone: "Europe/Berlin",
+  });
+  document.getElementById("time").innerHTML = timeLA;
+}
+
+
